aa8c66e0b9bdcfac1f61d32eb7e5f1f78e47f89b
tutkuince <tutku.ince@outlook.com>
2018-08-17 16:46:35 +0300

    Fixed bugs

--- src/com/muditasoft/config/SpringConfig.java
+++ src/com/muditasoft/config/SpringConfig.java
@@ -17,7 +17,7 @@ public class SpringConfig implements WebMvcConfigurer {
 	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		viewResolver.setPrefix("WEB-INF/view/");
+		viewResolver.setPrefix("/WEB-INF/view/");
 		viewResolver.setSuffix(".jsp");
 
 		return viewResolver;
