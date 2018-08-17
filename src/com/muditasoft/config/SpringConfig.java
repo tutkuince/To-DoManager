760fdc67d82f6b9d8625910a30857bda98fba06f
tutkuince <tutku.ince@outlook.com>
2018-08-17 12:35:48 +0300

    Spring configuration file

---
+++ src/com/muditasoft/config/SpringConfig.java
@@ -0,0 +1,24 @@
+package com.muditasoft.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@ComponentScan("com.muditasoft")
+@Configuration
+public class SpringConfig implements WebMvcConfigurer {
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+
+		return viewResolver;
+	}
+}
