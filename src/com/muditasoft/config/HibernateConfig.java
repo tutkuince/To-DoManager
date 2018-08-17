a8365900dbc1441cf1564cce0ca17f1af0ae0777
tutkuince <tutku.ince@outlook.com>
2018-08-17 13:37:31 +0300

    declare LocalSessionFactoryBean

--- src/com/muditasoft/config/HibernateConfig.java
+++ src/com/muditasoft/config/HibernateConfig.java
@@ -1,6 +1,7 @@
 package com.muditasoft.config;
 
 import java.beans.PropertyVetoException;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
@@ -19,27 +21,27 @@ public class HibernateConfig {
 
 	@Autowired
 	private Environment environment;
-	
+
 	// Set up a Logger for diagnostics
-	private Logger logger = Logger.getLogger(getClass().getName()); 
+	private Logger logger = Logger.getLogger(getClass().getName());
 
 	@Bean(destroyMethod = "close")
 	public ComboPooledDataSource comboPooledDataSource() {
 		// Create Connection Pool
 		ComboPooledDataSource dataSource = new ComboPooledDataSource();
-		
+
 		// Get JDBC Properties
 		String url = environment.getProperty("jdbc.url");
 		String user = environment.getProperty("jdbc.user");
 		String password = environment.getProperty("jdbc.password");
 		String driver = environment.getProperty("jdbc.driver");
-		
+
 		// Get Connection Pool Properties
 		int initPoolSize = getIntProperty("connection.pool.initialPoolSize");
 		int minPoolSize = getIntProperty("connection.pool.minPoolSize");
 		int maxPoolSize = getIntProperty("connection.pool.maxPoolSize");
 		int maxIdleTime = getIntProperty("connection.pool.maxIdleTime");
-		
+
 		// Set JDBC properties to ComboPooledDataSource
 		dataSource.setJdbcUrl(url);
 		dataSource.setUser(user);
@@ -50,7 +52,7 @@ public class HibernateConfig {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		// Set Connection Pool Properties to ComboPooledDataSource
 		dataSource.setInitialPoolSize(initPoolSize);
 		dataSource.setMinPoolSize(minPoolSize);
@@ -60,9 +62,26 @@ public class HibernateConfig {
 		// Log to Connection Properties
 		logger.info("===>>> jdbc.url = " + url);
 		logger.info("===>>> jdbc.user = " + user);
-		
+
 		return dataSource;
 	}
+
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean bean = new LocalSessionFactoryBean();
+		bean.setDataSource(comboPooledDataSource());
+		bean.setPackagesToScan("com.muditasoft.model");
+
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+		properties.setProperty("hibernate.show_sql", "true");
+
+		bean.setHibernateProperties(properties);
+
+		return bean;
+	}
+
+	
 
 	public int getIntProperty(String name) {
 		// get property value
