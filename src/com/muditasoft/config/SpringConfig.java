1076ada40adb067021fa7d1086e7195b50085faf
tutkuince <tutku.ince@outlook.com>
2018-08-17 12:40:02 +0300

    added addViewControllers()

--- src/com/muditasoft/config/SpringConfig.java
+++ src/com/muditasoft/config/SpringConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -20,5 +21,10 @@ public class SpringConfig implements WebMvcConfigurer {
 		viewResolver.setSuffix(".jsp");
 
 		return viewResolver;
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index");
 	}
 }
